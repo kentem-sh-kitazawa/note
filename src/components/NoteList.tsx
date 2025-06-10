bfc37b68ec45d9777ea5c99998e7471d808fdf4f
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-10 10:50:03 +0900

    HTML要素を配置

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -3,4 +3,15 @@ const NoteList = () => {
   //ローカルストレージに配列として保存→その配列をmapで展開
   //stateが変更されるとリストの表示も変わる、年月日と時間の表示
   //追加を押したときに新しく作成、stateをリセット
+  return (
+    <div>
+      <li>
+        <h3>{title}</h3>
+        <p>{mainText}</p>
+        <p>日時</p>
+      </li>
+    </div>
+  );
 };
+
+export default NoteList;
