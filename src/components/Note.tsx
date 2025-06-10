aa164b736c577da62cc97192f54b00443b107e67
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-10 10:08:39 +0900

    タイトル、本文を保持するstateを作成

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,3 +1,9 @@
 import { useState } from "react";
 
-const Note = () => {};
+const Note = () => {
+  //タイトルを保持するstate
+  const [title, setTitle] = useState<string>("");
+  //本文を保持するstate
+  const [mainText, setMainText] = useState<string>("");
+  return <div></div>;
+};
