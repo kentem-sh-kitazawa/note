aeca8cfa5c71a0901002f1ffb5a0e73732c519ac
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-10 14:37:30 +0900

    ノートを追加する処理を作成

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -9,8 +9,24 @@ type Props = {
 
 const NoteList = ({ title, setTitle, mainText, setMainText }: Props) => {
   //stateが変更されるとリストの表示も変わる、年月日と時間の表示
+
+  const addNote = () => {
+    const note = {
+      title: title,
+      mainText: mainText,
+    };
+    const prevNotes = localStorage.getItem("notes");
+    const parseNotes = prevNotes ? JSON.parse(prevNotes) : [];
+    const updateNotes = [...parseNotes, note];
+    localStorage.setItem("notes", JSON.stringify(updateNotes));
+    setTitle("新しいノート");
+    setMainText("");
+  };
+
   return (
     <div>
+      <button onClick={() => addNote()}>追加</button>
+
       <li>
         <h3>{title}</h3>
         <p>{mainText}</p>
