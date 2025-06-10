8000b6bd9ddb6b280b3db61212693b032b599a52
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-10 15:40:53 +0900

    現在の日付、時間を取得する処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,20 @@ function App() {
   const [title, setTitle] = useState<string>("新しいノート");
   //本文を保持するstate
   const [mainText, setMainText] = useState<string>("");
+  //現在時刻の取得
+  const [date, setDate] = useState<string>();
+  const [time, setTime] = useState<string>();
+
+  const getDateTime = () => {
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDate();
+    setDate(year + "/" + month + "/" + day);
+    let hour = d.getHours().toString().padStart(2, "0");
+    let minute = d.getMinutes().toString().padStart(2, "0");
+    setTime(hour + ":" + minute);
+  };
 
   return (
     <div className="home-page">
