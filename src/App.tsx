eb9efe6270e6a6642b16cc2e465dcdbdbc8449af
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-10 10:44:32 +0900

    Noteからstateを移動、Propsとして渡すように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 import Note from "./components/Note";
 
 function App() {
+  //タイトルを保持するstate
+  const [title, setTitle] = useState<string>("新しいノート");
+  //本文を保持するstate
+  const [mainText, setMainText] = useState<string>("");
   return (
     <>
-      <Note />
+      <Note
+        title={title}
+        setTitle={setTitle}
+        mainText={mainText}
+        setMainText={setMainText}
+      />
     </>
   );
 }
