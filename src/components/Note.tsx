0b6d2aae9fa619698966c1703e84ad0020afc38d
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-11 14:26:22 +0900

    propsと型定義を変更

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,32 +5,15 @@ import type { NoteType } from "../Type/NoteType";
 import "../style/Note.css";
 
 type Props = {
-  title: string;
-  setTitle: Dispatch<SetStateAction<string>>;
-  mainText: string;
-  setMainText: Dispatch<SetStateAction<string>>;
-  dateTime: string;
-  getDateTime: () => void;
+  notes: NoteType[];
+  setNotes: Dispatch<SetStateAction<NoteType[]>>;
 };
 
-const Note = ({
-  title,
-  setTitle,
-  mainText,
-  setMainText,
-  dateTime,
-  getDateTime,
-}: Props) => {
-  const note: NoteType = {
-    title: title,
-    mainText: mainText,
-    dateTime: dateTime,
-  };
-
-  useEffect(() => {
-    getDateTime();
-    localStorage.setItem("note", JSON.stringify(note));
-  }, [title]);
+const Note = ({ notes, setNotes }: Props) => {
+  // useEffect(() => {
+  //   getDateTime();
+  //   localStorage.setItem("note", JSON.stringify(note));
+  // }, [title]);
 
   useEffect(() => {
     getDateTime();
