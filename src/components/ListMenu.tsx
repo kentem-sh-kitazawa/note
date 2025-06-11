a018cae8616bd4babeb4c77af67980eaa727d409
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-11 10:11:00 +0900

    ListMenuにスタイルを適用

--- src/components/ListMenu.tsx
+++ src/components/ListMenu.tsx
@@ -1,11 +1,13 @@
+import "../style/ListMenu.css";
+
 type Props = {
   addNote: () => void;
 };
 
 const ListMenu = ({ addNote }: Props) => {
   return (
-    <div>
-      <h2>ノート</h2>
+    <div className="list-menu">
+      <h1>ノート</h1>
       <button onClick={() => addNote()}>追加</button>
     </div>
   );
